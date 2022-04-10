063c514cffbf116ef22e2504c1a4a0e7fc409b6c
npnpnp000 <37494434+npnpnp000@users.noreply.github.com>
2022-04-10 23:21:12 +0300

    fix 1

--- app/src/main/java/com/questionnaire/application/MainApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.questionnaire.application
-
-import android.app.Application
-import com.questionnaire.model.Repository
-
-class MainApplication : Application() {
-    val repository by lazy { Repository() }
-}
